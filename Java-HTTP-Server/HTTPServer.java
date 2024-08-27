365fbd1c27be6c362ec886fc15d209af018c2d2a
Kristian Norved <kristiannorved@gmail.com>
2024-08-27 18:28:51 +1000

    Server now runs on localhost, also removed server.java

--- Java-HTTP-Server/HTTPServer.java
+++ Java-HTTP-Server/HTTPServer.java
@@ -1,11 +1,42 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class HTTPServer {
-    public static void main(String[] args) throws Exception {
-    final ServerSocket server = new ServerSocket(8080); // constant variable for server
-    System.out.println("Listening for connection on port 8080 ....");
-    while (true){
-      // spin forever
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening for connection on port 8080..."); 
+        while (true) {
+            try {
+                Socket socket = server.accept();
+                
+                InputStream in = socket.getInputStream();
+                OutputStream out = socket.getOutputStream();
+
+                String html = "<html><head><title>Simple HTTP Java Server</title></head><body><hl>This page was served using my Simple Java HTTP server</hl></body></html>";
+
+                final String CRLF = "\r\n"; // 13, 10
+
+                String response = 
+                    "HTTP/1.1 200 OK" + CRLF + // Status Line: HTTP_VERSION RESPONSE_CODE RESPONSE_MESSAGE
+                    "Content-Length: " + html.getBytes().length + CRLF + // HEADER
+                    CRLF + 
+                    html + 
+                    CRLF + CRLF;
+                out.write(response.getBytes());
+                
+                /*
+                in.close();
+                out.close();
+                socket.close();
+                server.close();
+                */
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
-  }
 }
