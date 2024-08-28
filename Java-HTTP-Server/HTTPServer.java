2d0310637789f54550feef4dc396c3d9520806fc
Kristian Norved <kristiannorved@gmail.com>
2024-08-28 13:27:14 +1000

    The server can now successfully read requests from the client. The next step is to handle said requests appropriately

--- Java-HTTP-Server/HTTPServer.java
+++ Java-HTTP-Server/HTTPServer.java
@@ -5,19 +5,25 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.List;
 
 public class HTTPServer {
     public static void main(String[] args) throws IOException {
         // start server on port 8080
         ServerSocket server = new ServerSocket(8080);
-        System.out.println("Listening for connection on port 8080..."); 
+        System.out.println("Opening server on localhost:8080");
 
         while (true) {
-            try {
-                // accept connection from a client
-                Socket socket = server.accept();
+            // accept connection from a client
+            Socket socket = server.accept();
+
+            // if connection is stable, read and write from client
+            try (OutputStream out = socket.getOutputStream()) {
                 InputStream in = socket.getInputStream();
-                OutputStream out = socket.getOutputStream();
+                var line = new BufferedReader(new InputStreamReader(in)).readLine();
+                System.out.println(line);
 
                 // load HTML file from file directory
                 StringBuilder contentBuilder = new StringBuilder();
@@ -45,9 +51,24 @@ public class HTTPServer {
                 // write HTML message to the client
                 out.write(response.getBytes());
 
-            } catch (IOException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    /*private static HttpReq readRequest(Socket socket) throws IOException {
+        try (var in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+            var line = in.readLine();
+            System.out.println("Request line = " + line);
+        } catch (IOException e) {
+            //e.printStackTrace();
+            System.out.println("Whoops!");
+        }
+        return null;
+    }*/
+
+    private record HttpReq(String method, String url, Map<String, List<String>> headers, byte[] body) {
+
     }
 }
